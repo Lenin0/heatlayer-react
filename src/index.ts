80fa59b5827338b95d79f8be676b22cc116ce7c0
Pedro lenin <pedrolenin06@gmail.com>
2026-05-27 18:05:20 -0300

    feat(index): export public heatmap library API

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { default as HeatmapChart } from "./heatmap";
+export { HeatmapImageMode } from "./heatmap.imageMode";
+
+export type {
+  HeatmapPoint,
+  HeatmapChartProps,
+  HeatmapImageModeProps,
+  MapConfig,
+  HeatMode,
+  RenderTooltip,
+  ImageRect,
+  Pan,
+} from "./types";
+
